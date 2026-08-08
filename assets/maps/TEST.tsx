e4459f3a68046badfbb2732382c06d2bbdc4846a
csutyak <csutyak@sancsoft.com>
2026-08-08 12:37:19 -0400

    update server cli to have more commands

---
+++ assets/maps/TEST.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="TEST" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="../tiles/grass_tileset_16x16.png" width="256" height="256"/>
+</tileset>
